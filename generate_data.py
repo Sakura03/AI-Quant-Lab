59e5dca2616fa727213a87ed5de52709552faf09
Xinyu Zhang <zhangxinyu@utopilot.ai>
2025-11-24 20:05:34 +0800

    Improve README and allow to set params in cmd line

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,6 @@
 from dataclasses import asdict
 
+import argparse
 import os
 import os.path as osp
 import pandas as pd
@@ -11,23 +12,33 @@ from nofx.utils import timeframe_to_seconds
 
 
 if __name__ == "__main__":
-    config_path = "configs/default.yml"
-    config = Config.parse_config_file(config_path)
+    parser = argparse.ArgumentParser(description="下载回测数据的脚本")
+    parser.add_argument("config", type=str, help="配置文件路径")
+    parser.add_argument("--start-time", default=None, type=str, help="数据的起始时间")
+    parser.add_argument("--end-time", default=None, type=str, help="数据的结束时间")
+    parser.add_argument("--symbols", nargs="*", default=[], help="交易对列表")
+    parser.add_argument("--timeframes", nargs="*", default=[], help="时间周期列表")
+    args = parser.parse_args()
+
+    config = Config.parse_config_file(args.config)
     logger = setup_logger("generate_data")
 
     os.makedirs(config.backtest.data_folder, exist_ok=True)
 
-    symbols = config.trader.symbols
-    timeframes = list(config.indicators.keys())
+    symbols = args.symbols or config.trader.symbols
+    timeframes = args.timeframes or list(config.indicators.keys())
     if "1m" not in timeframes:
         timeframes.append("1m")
 
+    start_time = args.start_time or config.backtest.start_time
+    end_time = args.end_time or config.backtest.end_time
+
     exchange = Exchange(**asdict(config.exchange), logger=logger)
 
-    for symbol in config.trader.symbols:
+    for symbol in symbols:
         symbol_future = symbol.replace("/", "")
         # Funding Rate
-        df = exchange.fetch_history_funding_rate(symbol_future, start_time=config.backtest.start_time, end_time=config.backtest.end_time)
+        df = exchange.fetch_history_funding_rate(symbol_future, start_time=start_time, end_time=end_time)
         df["timestamp"] = df["timestamp"].astype("int64") // int(1e6)
         filename = symbol.replace("/", "_") + "-funding-rate.feather"
         save_path = osp.join(config.backtest.data_folder, filename)
@@ -37,8 +48,7 @@ if __name__ == "__main__":
         for timeframe in timeframes:
             # 多存1000个周期的数据, 为了计算技术指标
             period = pd.Timedelta(seconds=timeframe_to_seconds(timeframe))
-            start_time = config.backtest.start_time - 1000 * period
-            df = exchange.fetch_history_ohlcv_df(symbol_future, timeframe, start_time=start_time, end_time=config.backtest.end_time)
+            df = exchange.fetch_history_ohlcv_df(symbol_future, timeframe, start_time=start_time - 1000 * period, end_time=end_time)
             df["timestamp"] = df["timestamp"].astype("int64") // int(1e6)
             filename = symbol.replace("/", "_") + "-" + timeframe + ".feather"
             save_path = osp.join(config.backtest.data_folder, filename)
